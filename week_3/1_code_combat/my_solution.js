a09f7e089bec8ca19f530ede0e93778454c5a381
Joey Rosztoczy <joeyr@stotzeq.com>
2014-04-15 14:22:09 -0700

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -5,12 +5,77 @@
 // command + / This will comment the line. 
  
  
+ // 1. Rescue Mission: Get behind the Ogre
+ //  1.1 Move up
+ //  1.2 Move right x 3 
+ //  1.3 Move down x 2
+  
+ // 2. Get the Mushroom
+ //  1.1 Move up
+ //  1.2 Move right
+ //  1.3 Move left
+ //  1.4 Move up
+ //  1.5 Attack Ogre
+  
+ // 3. Drink me 
+ //  1.1 Move right
+ //  1.2 Attach Ogre 1
+ //  1.3 Move right
+ //  1.4 Move down
+ //  1.5 move up
+ //  1.6 move right
+ //  1.7 attack ogre 2
+  
+ // 4. Taunt the Guards 
+ //  1.1 Move right
+ //  1.2 Taunt Ogre 1
+ //  1.3 Move Left
+ //  1.4 Shoot Ogre
+ //  1.5 move right
+ //  1.6 move up
+ //  1.7 Run around ogres
+ 
+ // 5. Its a trap
+ //  1.1 Move down x 2
+ //  1.2 Taunt Ogre 1
+ //  1.3 Move up x 2
+  
+ // 6. Taunt
+ //  6.1 Four separate insults 
+  
+ // 7. Luring 
+ //  7.1 Move to middle
+ //  7.2 Taunt the ogres
+ //  7.3 Run for cover
+  
+ // 8. Attacking with an army
+ //  8.1 Get homies to follow
+ //  8.2 Run at enemy
+ //  8.3 Get friends to attack enemy
+  
+ // 9. Attach with Mobile Artillary 
+ //  9.1 Kill small ogres
+ //  9.2 Move to range of second group
+ //  9.3 Fire two shots one at start and one midway
+ //  9.4 Move to third group
+ //  9.5 Fire two shots one at start and one midway
+
  
  
- 
+ // Questions
+ // this refers to an object itself once it has been selected
+ // () calls a function in js
+ // The semicolons allow for multiple lines of code to run, I believe it breaks after code is executed.
  
  
  
  
 // Reflection:
 // Write your reflection here.
+
+// The exercises were straight forward and simple. I appreciated the analogies to gaming ( I am a big gamer). I didn't really run
+// into too many challenges. I liked the time breakdown, similar to stepping through code as you debug it. I didn't have any 
+// outside questions from the exercises. I had used js before so not too much learned other than that using this is similar to 
+// Ruby. The last challenge was by far the most fun because you weren't boxed in by walls and could be as creative 
+// as you wanted to be. Not being able to see the end goal in some of the challenges until halfway through was 
+// the most tedious part for sure.
